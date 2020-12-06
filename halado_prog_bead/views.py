45ed0e770efd0d47042f411b9543e59a30124977
DjSall <proksalevente@gmail.com>
2020-12-06 22:25:39 +0100

    signup view completed

--- halado_prog_bead/views.py
+++ halado_prog_bead/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpRequest;
 from django.template import Context
 
 # example post data - TODO: Convert to DICT ARRAY so multiple posts can be viewed
+from .forms import SignUpForm
+
 article_data = {
 	"title": "yeah, here we go",
 	"content": "Fuck you",
@@ -41,6 +45,18 @@ def submitpost(request):
 
 # signup form
 def signup(request):
+	form = UserCreationForm(request.POST)
+	if form.is_valid():
+		form.save()
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password1')
+		user = authenticate(username=username, password=password)
+		login(request, user)
+		return redirect('index')
+	else:
+		form = SignUpForm()
+	return render(request, 'registration/signup.html', {'form': form})
+
 	return render(
 		request,
 		'registration/signup.html'
