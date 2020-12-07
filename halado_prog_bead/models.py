7792d2b57892c114b6302c2c32019585aacba170
DjSall <proksalevente@gmail.com>
2020-12-07 17:15:45 +0100

    models conflict ffs

--- halado_prog_bead/models.py
+++ halado_prog_bead/models.py
@@ -1,16 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.db import models
+
+
 class Post(models.Model):
     Text = models.CharField(max_length=16384)
     Author = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     Time = models.DateTimeField()
-
-
-@receiver(post_save, sender=User)
-def update_profile_signal(sender, instance, created, **kwargs):
-	if created:
-		Profile.objects.create(user=instance)
-	instance.profile.save()
-
