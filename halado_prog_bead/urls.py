59507c1923fce23b5cd2344908fbd038e421f21c
DjSall <proksalevente@gmail.com>
2020-12-07 15:47:04 +0100

    login now works, signup still broken

--- halado_prog_bead/urls.py
+++ halado_prog_bead/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from halado_prog_bead import views
+
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
@@ -14,15 +17,8 @@ urlpatterns += [
 	path('viewposts', views.viewposts, name='viewposts'),
 	path('submitpost', views.submitpost, name='submitpost'),
 	path('signup', views.signup, name='signup'),
+	path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 # static files
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# login logout
-urlpatterns += [
-	path('accounts/', include('django.contrib.auth.urls')),
-]
